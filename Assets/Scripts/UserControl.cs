3998f8ce2b3246db22ba4e6bd47df531c44c026e
mbu1234 <89271696+mbu1234@users.noreply.github.com>
2023-12-28 20:25:28 +0000

    Testing object-orientated principles
    Making changes to implement some more abstraction, polymorphism + inheritance along with encapsulation (getters, setters) - some validation added to a setter ....

--- Assets/Scripts/UserControl.cs
+++ Assets/Scripts/UserControl.cs
@@ -20,30 +20,8 @@ public class UserControl : MonoBehaviour
         Marker.SetActive(false);
     }
 
-    private void Update()
-    {
-        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        GameCamera.transform.position = GameCamera.transform.position + new Vector3(move.y, 0, -move.x) * PanSpeed * Time.deltaTime;
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                //the collider could be children of the unit, so we make sure to check in the parent
-                var unit = hit.collider.GetComponentInParent<Unit>();
-                m_Selected = unit;
-                
-                
-                //check if the hit object have a IUIInfoContent to display in the UI
-                //if there is none, this will be null, so this will hid the panel if it was displayed
-                var uiInfo = hit.collider.GetComponentInParent<UIMainScene.IUIInfoContent>();
-                UIMainScene.Instance.SetNewInfoContent(uiInfo);
-            }
-        }
-        else if (m_Selected != null && Input.GetMouseButtonDown(1))
-        {//right click give order to the unit
+    public void HandleAction() {
+        //right click give order to the unit
             var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
@@ -59,6 +37,37 @@ public class UserControl : MonoBehaviour
                     m_Selected.GoTo(hit.point);
                 }
             }
+    }
+
+    public void HandleSelection() {
+        var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //the collider could be children of the unit, so we make sure to check in the parent
+                var unit = hit.collider.GetComponentInParent<Unit>();
+                m_Selected = unit;
+                
+                
+                //check if the hit object have a IUIInfoContent to display in the UI
+                //if there is none, this will be null, so this will hid the panel if it was displayed
+                var uiInfo = hit.collider.GetComponentInParent<UIMainScene.IUIInfoContent>();
+                UIMainScene.Instance.SetNewInfoContent(uiInfo);
+            }
+    }
+
+    private void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        GameCamera.transform.position = GameCamera.transform.position + new Vector3(move.y, 0, -move.x) * PanSpeed * Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleSelection();
+        }
+        else if (m_Selected != null && Input.GetMouseButtonDown(1))
+        {
+            HandleAction();
         }
 
         MarkerHandling();
